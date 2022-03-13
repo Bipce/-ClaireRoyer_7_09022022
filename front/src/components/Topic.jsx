f5f7a3e92ac5dbb29bc4a47939ed779925331090
Bipce <claireroyer57.com>
2022-03-13 23:13:17 +0100

    Add axios

--- front/src/components/Topic.jsx
+++ front/src/components/Topic.jsx
@@ -1,19 +1,22 @@
 import "./Topic.css";
 import "../styles/buttons.css";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import Axios from "axios";
+import { useState } from "react";
 
 const Topic = () => {
-  useEffect(() => {
-    fetch("http://localhost:3001/api/topics").then((res) =>
-      res
-        .json()
-        .then((topics) => {
-          console.log(topics);
-        })
-        .catch((err) => console.log(err))
-    );
-  }, []);
+  const [topics, setTopics] = useState(false);
+
+  const getTopics = () => {
+    Axios.get("http://localhost:3001/api/topics").then((response) => {
+      console.log(response.data.map((topics) => <p key={topics.id}></p>));
+      setTopics(
+        response.data.map((topics) => (
+          <h1 key={topics.id}>Title: {topics.title}</h1>
+        ))
+      );
+    });
+  };
 
   return (
     <div className="topic">
@@ -22,7 +25,7 @@ const Topic = () => {
           <p>Pseudo+heure</p>
         </div>
         <div className="topic__title-content">
-          <h1 className="topic__title">title</h1>
+          <h1 className="topic__title">Title: {topics.title}</h1>
           <div>
             Text/Image Text/Image Text/Image Text/Image Text/Image Text/Image
             Text/Image Text/Image Text/Image Text/Image Text/ImageText/Image
